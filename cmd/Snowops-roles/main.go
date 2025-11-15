3416b9f43597fc477114fba60a5a556f91436e1f
mccnlight <nurpeissov.a0818@gmail.com>
2025-11-15 19:30:12 +0500

    allow cors

--- cmd/Snowops-roles/main.go
+++ cmd/Snowops-roles/main.go
@@ -4,10 +4,12 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/MSTimX/Snowops-roles/internal/database"
 	"github.com/MSTimX/Snowops-roles/internal/handlers"
 	"github.com/MSTimX/Snowops-roles/internal/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -30,6 +32,15 @@ func main() {
 	}
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"*"},
+		ExposeHeaders: []string{
+			"Content-Type",
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	router.GET("/healthz", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "ok"})
